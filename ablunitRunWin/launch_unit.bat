04eaa20aa2ecbbed88c9ef716256650de701f215
Justas.Poliakas <Justas.Poliakas@consult.luminorgroup.com>
2020-10-14 10:47:42 +0300

    bug fix for incorrect results.xml path

--- ablunitRunWin/launch_unit.bat
+++ ablunitRunWin/launch_unit.bat
@@ -4,7 +4,7 @@ echo Starting tests...
 
 C:\Progress\OpenEdge\bin\prowin.exe -b -basekey "INI" -ininame propath.ini -p ABLUnitCore.p -param "CFG=./ablunit.json"
 
-findstr /m "</failure>" results.xml > nul
+findstr /m "</failure>" ..\results.xml > nul
 if %errorlevel%==0 (
 echo Seems there are failing tests...
 )
